3f7b509db076bb1823c84c12e2b832a828f78f6e
jl2286716 <jleduc@student.rcc.edu>
2014-08-30 20:27:04 -0700

    Signed-off-by: jl2286716 <jleduc@student.rcc.edu>

--- Homework/Leduc, James - Assignment 1 Review - 48130.cpp
+++ Homework/Leduc, James - Assignment 1 Review - 48130.cpp
@@ -42,7 +42,8 @@ int main(){
 		cout << setprecision(2) << fixed << showpoint;
 		
 		// Display results.
-		cout << endl << celc << " degrees Celsius equals " << fahr << " degrees Fahrenheit.";
+		cout << endl << celc << " degrees Celsius equals " 
+		<< fahr << " degrees Fahrenheit." << endl;
 		
 		return 0;
 	}
@@ -75,7 +76,25 @@ int main(){
 		cout << setprecision(2) << fixed << showpoint;
 		
 		// Display results.
-		cout << endl << "$" << dollars << " is " << euroConvert << " Euros and " << yenConvert << " Japanese Yen.";
+		cout << endl << "$" << dollars << " is equal to " 
+		<< euroConvert << " Euros and " << yenConvert << " Japanese Yen." << endl;
 		
 		return 0;	
+	}
+	
+// Chap04_Prob10: Days in a Month
+	// Write a program that asks the user to enter the month (letting the user enter an integer
+	// in the range of 1 through 12) and the year. The program should then display the number of
+	// days in that month. Use the following criteria to identify leap years:
+		// 1. Determine whether the year is divisible by 100. If it is, then it is a leap year if
+		// 	and only if it is divisible by 400. For example, 2000 is a leap year but 2100 is not.
+		// 2. If the year is not divisible by 100, then it is a leap year if and only if it is
+		// 	divisible by 4. For example, 2008 is a leap year but 2009 is not.
+	// Here is a sample run of the program:
+		// Enter a month (1-12): 2[Enter]
+		// Enter a year: 2008[Enter]
+		// 29 days
+	int Chap04_Prob10(){
+	
+		return 0:
 	}
