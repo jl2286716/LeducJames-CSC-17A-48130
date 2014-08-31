c97bf2589500cd3d204739d26181e9d47a125cde
jl2286716 <jleduc@student.rcc.edu>
2014-08-30 19:01:32 -0700

    Signed-off-by: jl2286716 <jleduc@student.rcc.edu>

---
+++ Homework/Leduc, James - Assignment 1 Review - 48130.cpp
@@ -0,0 +1,18 @@
+/*
+	Programmer: James Leduc
+	Program Name: Gaddis_8thEd_Assignment01
+	Date Created: 08/30/2014
+	Date Modified: 08/30/2014
+	Purpose: A menu driven program to run scripts from chapters 3-8 in
+		"Starting Out With C++: From Control Structures Through Objects"
+		by Tony Gaddis, 8th Edition:
+		3.12, 3.13, 4.10, 5-11, 6.7, 7.6, 8.7
+		Two problems from chapters 5 through 8 should include flowcharts.
+*/
+
+#incluse <iostream>
+using namespace std;
+
+int main(){
+
+)
