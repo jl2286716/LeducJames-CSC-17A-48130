b069c0c6fd3af7874ff4fbd5c051250efa274552
jl2286716 <jleduc@student.rcc.edu>
2014-09-09 10:36:34 -0700

    Signed-off-by: jl2286716 <jleduc@student.rcc.edu>

--- Homework/Assignment01/Leduc, James - Assignment 1 Review - 48130.cpp
+++ Homework/Assignment01/Leduc, James - Assignment 1 Review - 48130.cpp
@@ -378,7 +378,6 @@ int main(){
 			cout << "File opening failed!";
 		}
 
-		
 		for(int i = 0; i < ROWS; i++)
 		{
 			for(int j = 0; j < COLS; j++)
@@ -496,4 +495,27 @@ int main(){
 		
 		system("pause");
 		system("cls");
+	}
+
+	// The Binary Search Function
+	sring binarySearch(const string array[], int numElems, int value){
+		int first = 0,				// First array element.
+			last = numElems - 1,	// Last array element.
+			middle,					// Midpoint of search.
+			position = -1;			// Position of search value.
+		bool found = false;			// Flag.
+
+		while (!found && first <= last){
+			middle = (first + last) / 2;		// Calculate midpoint.
+			if (array[middle] == value){		// If value is found at mid.
+				found = true;
+				position = middle;
+			}else if (array[middle] > value){	// If value is in lower half.
+				last = middle - 1;
+			}else{
+				first = middle + 1;				// If value is in upper half.
+			}
+		}
+		return position;
+
 	}
