72c2beba12ecb8cf93322c715f85b9d5eff90b04
jl2286716 <jleduc@student.rcc.edu>
2014-09-30 10:55:33 -0700

    Signed-off-by: jl2286716 <jleduc@student.rcc.edu>

--- Homework/Assignment02-Ch10&12_Challenges/Chapter12.cpp
+++ Homework/Assignment02-Ch10&12_Challenges/Chapter12.cpp
@@ -122,3 +122,24 @@ void menu(int c){
 //		of the file it is passed (the punch line file). It should find this line by seeking 
 //		to the end of the file and then backing up to the beginning of the last line. Data 
 //		to test your program can be found in the joke.txt and punchline.txt files.
+
+
+//	Programming Challenge #4. Tail Program
+//		Write a program that asks the user for the name of a file. The program should display 
+//		the last 10 lines of the file on the screen (the "tail" of the file). If the file has 
+//		fewer than 10 lines, the entire file should be displayed, with a message indicating 
+//		the entire file has been displayed.
+
+
+//	Programming Challenge #5. Line Numbers
+//		(This assignment should be done as a modification to Programming Challenge 2.)
+//		Write a program that asks the user for the name of a file. The program should display 
+//		the contents of the file on the screen. Each line of scree output should be preceded 
+//		with a line number, followed by a colon. The line numbering should start at 1. 
+//		Here is an example:
+//			1:George Rolland
+//			2:127 Academy Street
+//			3:Brasstown, NC 28706
+//		If the file's contents won't fit on a sigle screen, the program should display 24 
+//		lines of output at a time, and then pause. Each time the program pauses, it should 
+//		wait for the user to strike a key before the next 24 lines are displayed.
