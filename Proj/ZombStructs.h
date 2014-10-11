9f7a3533346e4e1acb99a607494b40a3f6c2d2ff
jl2286716 <jleduc@student.rcc.edu>
2014-10-11 13:07:22 -0700

    Signed-off-by: jl2286716 <jleduc@student.rcc.edu>

--- Proj/ZombStructs.h
+++ Proj/ZombStructs.h
@@ -1,49 +1,4 @@
-struct Equipment{	//	Holds storage and armor.
-	string type;	//	type1 = {armor,store,quiv}
-	string wear;	//	wear = {chest,feet,back,waist}
-	int effect1;	//	effect1 = health(chest,feet)/load(back,waist)
-	int effect2;	//	effect2 = speed
-	int weight;		//	weight effects the load
-};
-
-struct Ammo{
-	string type1;	//	type1 = supply
-	string type2;	//	type2 = {round,box,arrow} - rounds -> pocket, arrows -> quiver, box -> bag
-	int qty;		//	qty = quantity
-	string wear;	//	wear = {pocket,bag,quiver}
-	int weight;		//	weight effects the load
-};
-
-struct Regen{
-	string type1;	//	type1 = supply
-	string type2;	//	type2 = {heal,regen}
-	string wear;	//	wear = bag
-	int effect;		//	effect1 = +health
-	int weight;		//	weight effects the load
-};
-
-struct Item{
-	string type1;	//	type1 = supply
-	string type2;	//	type2 = item
-	string wear;	//	wear = {pocket,bag}
-	string wield;	//	wield = {NULL,single}
-	int weight;		//	effects the load
-};
-
-struct Weapon{
-	string type1;	//	weapon
-	string type2;	//	supply,NULL
-	string wear;	//	bag
-	string wield;	//	single,double
-	int effect1;	//	power
-	int effect2;	//	speed
-	string aType1;	//	crit = power*1.5
-	int accVal1;	//	% chance
-	string aType2;	//	insta,jam
-	int accVal2;	//	% chance
-	int weight;		//	load
-};
-
+/*	Create the Player	*/
 struct Player{
 	string name;
 	int health;		//	=200
@@ -56,6 +11,73 @@ struct Player{
 	int arLoad;		// 	=0
 	int arLoadBase;	//	=0
 };
+string getName(){
+	string pName;
+	cout << "What's your name?: ";
+	cin >> pName;
+	cout << endl << endl << "Good luck, " << pName << "!\n\n";
+	return pName;
+}
+/*returnType*/ setPlayer(name){
+	const int P_SIZE = 9;
+	int player[P_SIZE];
+	player[0]=200;
+	player[1]=200;
+	player[2]=50;
+	player[3]=20;
+	player[4]=20;
+	player[5]=25;
+	player[6]=25;
+	player[7]=0;
+	player[8]=0;
+	
+	//	Fill the 'Player' structure
+	
+	//	Return the 'Player' structure
+}
+
+struct Equipment{	//	Holds storage and armor.
+	string type;	//	type = {armor,store,quiv}
+	string wear;	//	wear = {chest,feet,back,waist}
+	int eff1;		//	eff1 = health(chest,feet)/load(back,waist)
+	int eff2;		//	eff2 = speed
+	int weight;		//	weight effects the load
+};
+
+struct Ammo{
+	string type;	//	type = {round,box,arrow} - rounds -> pocket, arrows -> quiver, box -> bag
+	int qty;		//	qty = quantity
+	string wear;	//	wear = {pocket,bag,quiver}
+	int weight;		//	weight effects the load
+};
+
+struct Regen{
+	string type;	//	type = {heal,regen}
+	string wear;	//	wear = bag
+	int effect;		//	effect1 = +health
+	int weight;		//	weight effects the load
+};
+
+struct Item{
+	string type;	//	type = item
+	string wear;	//	wear = {pocket,bag}
+	string wield;	//	wield = {NULL,single}
+	int weight;		//	effects the load
+};
+
+struct Weapon{
+	string type;	//	type = weapon
+	string wear;	//	wear = bag
+	string wield;	//	wield = {single,double}
+	int effect1;	//	effect1 = +power
+	int effect2;	//	effect2 = +speed
+	string accTyp1;	//	accTyp1 = crit{power*1.5}
+	int accVal1;	//	accVal1 = % chance
+	string accTyp2;	//	accTyp2 = {insta,jam}
+	int accVal2;	//	accVal2 = % chance
+	int weight;		//	weight = -load
+};
+
 
 struct Member{		//	used for team mates and villains
 	string name;
@@ -87,35 +109,40 @@ struct MemInv{		//	team member inventory
 
 //	Armor Items
 	//	T-Shirt
-		type 	= armor;
-		wear 	= chest;
-		effect1 = +1 health base;
-		effect2 = 0 speed;
-		weight 	= -1 load;
+		Equipment tShrt[5];
+			tShrt.type="armor";
+			tShrt.wear="chest";
+			tShrt.eff1=healthBase+1;
+			tShrt.eff2=speed-0;
+			tShrt.weight=load-1;
 	//	Coat
-		type 	= armor;
-		wear 	= chest;
-		effect1 = +10 health base;
-		effect2 = -2 speed;
-		weight 	= -2 load;
+		Equipment coat[5];
+			coat.type="armor";
+			coat.wear="chest";
+			coat.eff1=healthBase+10;
+			coat.eff2=speed-2;
+			coat.weight=load-2;
 	//	Teflon Vest
-		type 	= armor;
-		wear 	= chest;
-		effect1 = +50 health base;
-		effect2 = -3 speed;
-		weight 	= -10 load;
+		Equipment teflon[5];
+			teflon.type="armor";
+			teflon.wear="chest";
+			teflon.eff1=healthBase+50;
+			teflon.eff2=speed-3;
+			teflon.weight=load-10;
 	//	Kevlar Vest
-		type 	= armor;
-		wear 	= chest;
-		effect1 = +75 health base;
-		effect2 = -1 speed;
-		weight 	= -15 load;
+		Equipment kevlar[5];
+			kevlar.type="armor";
+			kevlar.wear="chest";
+			kevlar.eff1=healthBase+75;
+			kevlar.eff2=speed-1;
+			kevlar.weight=load-13;
 	//	Bare Feet
-		type 	= armor;
-		wear 	= feet;
-		effect1 = -5 health base;
-		effect2 = -5 speed;
-		weight 	= 0 load;
+		Equipment bare[5];
+			bare.type="armor";
+			bare.wear="feet";
+			bare.eff1=healthBase-5;
+			bare.eff2=speed-5;
+			bare.weight=load-0;
 	//	Slippers
 		type 	= armor;
 		wear 	= feet;
@@ -180,15 +207,21 @@ struct MemInv{		//	team member inventory
 		weight	= -10 load;
 
 //	Ammo Items
+	const int AMSIZE=5;
 	//	.357mm Round
+		rnd357[AMSIZE];
+		rnd357[0]="supply";
+		rnd357[1]="
 	//	.39mm Round
+		rnd39[AMSIZE];
 	//	.9mm Round
+		rnd9[AMSIZE];
 	//	Shotgun Shell
 		type1 	= supply;	//	type1 = supply
 		type2 	= round;	//	type2 = {round,box,arrow} - round -> pocket, arrows -> quiver, box -> bag
 		qty 	= 1;		//	qty = quantity
 		wear 	= pocket;	//	wear = {pocket,bag,quiver}
-		weight 	= -1 load;	//	weight effects the load
+		weight 	= -1 load;	//	weight = -load
 	//	Arrow
 		type1 	= supply;
 		type2 	= arrow;
@@ -245,7 +278,7 @@ struct MemInv{		//	team member inventory
 		type1	= supply;
 		type2	= regen;
 		wear	= waist;
-		effect2	= +1 health per;
+		effect2	= +2 health per;
 		weight	= -2 load;
 	//	water - Bottle Water
 		type1 	= supply;
