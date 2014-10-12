8498233fe740c2bec84461d748ed787557969031
jl2286716 <jleduc@student.rcc.edu>
2014-10-11 22:31:45 -0700

    Signed-off-by: jl2286716 <jleduc@student.rcc.edu>

--- Proj/iZombie.cpp
+++ Proj/iZombie.cpp
@@ -3,54 +3,53 @@
 
 using namespace std;
 
+void showIntro();
+
 int main(){
-	cout << "For the past few days, there have been news reports\n"
-		<< "of a new global viral pandemic sweeping across the nation...\n";
-	system("pause");
-	system("cls");
-	cout << "The time is 9:42 PM on a Sunday night.\n\n"
-		<< "You are at home in bed watching your favorite TV show\n"
-		<< "when it is interrupted by a breaking news bulletin...\n";
-	system("pause");
-	system("cls");
-	cout << "Reporter:\n"
-		<< "Chaos has erupted in Los Angeles and the surrounding counties!\n"
-		<< "We now go live to our correspondent on the scene!\n\n";
-	system("pause");
-	cout << "Correspondent:\n"
-		<< "-shrk-...pandemonium...-shrk-...are attacking other people...\n"
-		<< "-shrk-...dead...-shrk-...rising and...\n"
-		<< "-shrk-AAAAAHHH!!!-shrk-schhh-\n";
-	system("pause");
-	system("cls");
-	cout << "Reporter:\n"
-		<< "Oh! Oh my God! Wha... Was that blood that just spla...\n"
-		<< "Wait! This just in! The president is issuing a statement.\n"
-		<< "We now go live to the White House.\n\n";
-	system("pause");
-	cout << "President:\n"
-		<< "...remain indoors!-shrk-\n"
-		<< "...of rations and board up all...-shrk-schhh-\n\n";
-	system("pause");
-	cout << "Reporter:\n"
-		<< "We seem to have lost the feed and...\n"
-		<< "-yelling in the background-\n"
-		<< "...OH MY GOD!!!-shrk-schhh-\n\n";
-	system("pause");
-	system("cls");
-	cout << "The television turns to static.\n"
-		<< "You turn to the other channels for further info...\n"
-		<< "Nothing but static.\n";
-	system("pause");
-	cout << "You pick up your cellphone to check on your family and friends...\n"
-		<< "No service.\n";
-	system("pause");
-	cout << "You walk into the living room when you hear an explosion in the distance.\n"
-		<< "The windows shake and the power goes out.\n";
-	system("pause");
-	cout << "You hear faint screams accompanied by the neighborhood dogs barking frantically.\n"
-		<< "The screams grow nearer!\n\n";
-	system("pause");
-	system("cls");
+	showIntro();
 	cout << "Level 1: Get the Hell Outta There!\n\n";
+	
+	return 0;
+}
+
+void showIntro(){
+	ifstream inFile;
+	int linePer=22,	//	Number of lines printed per screen.
+		lineCnt=0;	//	Line counter initialized to zero.
+	string line;	//	Stores the lines to be written to the console.
+	
+	//	Open the 'intro.txt' file.
+	inFile.open("intro.txt",ios::in);
+	if(inFile.fail()){
+		cout << "The introduction is broken!\n";
+	}
+
+	//	Count the number of lines.
+	while(!inFile.eof()){
+		getline(inFile,line,'\n');
+		lineCnt++;
+	}
+
+	//	Go back to the beninning.
+	inFile.clear();
+	inFile.seekg(0L,ios::beg);
+
+	//	Iterate through and display.
+	while(!inFile.eof()){
+		if(lineCnt<linePer){
+			for(int j=0;j<lineCnt;j++){
+				getline(inFile,line,'\n');
+				cout << line << endl;
+			}
+			system("pause");
+		}else if(lineCnt>=linePer){
+			for(int i=0;i<linePer;i++){
+				getline(inFile,line,'\n');
+				cout << line << endl;
+				lineCnt--;
+			}
+			system("pause");
+		}
+	}
+	inFile.close();
 }
